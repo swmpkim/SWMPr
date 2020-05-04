1f4c2bf41752403865b468e4c5cbe7d3df042239
swmpkim <kimberly.cressman@dmr.ms.gov>
2020-05-04 16:03:32 -0500

    updating and checking par model function

--- R/calc_modeled_par.R
+++ R/calc_modeled_par.R
@@ -13,12 +13,15 @@
 #'
 #' @concept analyze
 #'
-#' @return
 #' @export
 #' 
-#' @details This function models photosynthetically active radiation (PAR) for a given location, at a given point in time, based on other meteorological parameters that can affect radiation: day of year (which influences sun angle), temperature, relative humidity, and barometric pressure.  I ASSUMED SEA LEVEL; ELEVATION IS SOMETHING THAT MATTERS IN THE MODEL MORE BROADLY
+#' @details This function models photosynthetically active radiation (PAR) for a given location, at a given point in time, based on other meteorological parameters that can affect radiation: day of year (which influences sun angle), temperature, relative humidity, and barometric pressure.  
+#' 
+#' I THINK that I used barometric pressure in lieu of elevation (which is what the website uses). CHECK ON THIS. 
+#' 
+#' Results are about 1.5% different than those from the clearskycalculator website, presumably due to rounding.
 #'
-#' @return a single value. units are mmol/m^2/s
+#' @return a single value. units are micromoles/m^2/s  (NOTE that SWMP PAR data values are totals for 15-minute PAR, in mmol/15-min. To convert between units: umol/m^2/s = 1.11 * mmol/15-min)  
 #' 
 #' @references This is based on the model used on clearskycalculator.com.
 #' website: http://clearskycalculator.com/quantumsensor.htm
@@ -28,14 +31,31 @@
 #' https://www.apogeeinstruments.com/content/EWRI-ASCE-Reference-ET-Appendices.pdf
 #'
 #'
-#' @examples
+#' @examples  
+#' # get location info for apaebmet
+#' apaeb_loc <- stat_locs[stat_locs$station_code == "apaeb", ]
+#' 
+#' # calculate modeled PAR for noon on Jan. 1st, 2012; 
+#' # actual was 903.1 mmol/15-min; this converts to 1002 umol/m^2/s
+#' calc_modeled_par(doy = 1, time_hrs = 12, 
+#'                  rh = 98, temp = 16.9, bp_mb = 1024,
+#'                  lat_decdeg = apaeb_loc$latitude,
+#'                  long_decdeg = -1 * apaeb_loc$longitude,
+#'                  longTZ = 75)
+#'                  
+#' # calculate for noon on June 1st, 2012; 
+#' # actual was 1843.1 mmol/15-min; this converts to 2045.8 umol/m^2/s  
+#' calc_modeled_par(doy = 152, time_hrs = 12, 
+#'                  rh = 64, temp = 27.8, bp_mb = 1011,
+#'                  lat_decdeg = apaeb_loc$latitude,
+#'                  long_decdeg = -1 * apaeb_loc$longitude,
+#'                  longTZ = 75)
 calc_modeled_par <- function(doy, time_hrs,
                              rh, temp, bp_mb,
                              lat_decdeg, long_decdeg, longTZ){
   
   # doy and time_hrs need to be pre-calculated
-  # maybe later I can get this to use POSIXct to automate it, but not now
-  
+
   # unit conversions
   bp_kPa <- bp_mb / 10
   lat_rad <- lat_decdeg * pi / 180
@@ -43,12 +63,12 @@ calc_modeled_par <- function(doy, time_hrs,
   # intermediate calculations
   # easy ones
   eT <- 0.6108 * exp( (17.27*temp) / (temp + 237.3) )
-  ea <- (rh / 100) * eT
-  prcp_water <- (0.14 * ea * bp_kPa) + 2.1
+  ea <- (rh / 100) * eT  # air vapor pressure
+  prcp_water <- (0.14 * ea * bp_kPa) + 2.1  # precipitable water in atmosphere
   
   # intermediate: Ra
   solar_decl <- 0.409 * sin( ((2*pi*doy)/365) - 1.39 )  # solar angle
-  dr <- 1 + 0.033 * cos(2*pi*doy / 365)
+  dr <- 1 + 0.033 * cos(2*pi*doy / 365) # inverse relative distance factor for the distance between Earth and sun
   Gsc <- 1361  # solar constant, W/m^2
   ws <- acos( -tan(lat_rad) * tan(solar_decl) )   # sunset hour angle, radians
   b <- 2*pi*(doy-81)/364
@@ -61,19 +81,22 @@ calc_modeled_par <- function(doy, time_hrs,
   if(w1 > ws){w1 <- ws}
   if(w2 > ws){w2 <- ws}
   if(w1 > w2){w1 <- w2}
-  Ra <- 12/pi * Gsc * dr * ( ((w2-w1)*sin(lat_rad)*sin(solar_decl)) + (cos(lat_rad)*cos(solar_decl)*(sin(w2)-sin(w1)) ) )
+  Ra <- 12/pi * Gsc * dr * ( ((w2-w1)*sin(lat_rad)*sin(solar_decl)) + (cos(lat_rad)*cos(solar_decl)*(sin(w2)-sin(w1)) ) )  # 	extraterrestrial radiation, Watts/m?
   
   # Ra to Rso
   sin_sun <- sin(lat_rad)*sin(solar_decl) + cos(lat_rad)*cos(solar_decl)*cos(w)
   aa <- -0.00146*bp_kPa/sin_sun
   bb <- 0.075*(prcp_water/sin_sun)^0.4
-  KB <- 0.98 * exp( aa - bb )
-  KD <- 0.35 - 0.36*KB
+  KB <- 0.98 * exp( aa - bb )  # clearness index for direct beam radiation
+  KD <- 0.35 - 0.36*KB  # transmissivity index for diffuse radiation
   Rso <- (KB + KD) * Ra
   
   # unit conversion: Rso above is W/m^2 total solar radiation
   # we need micromoles/m^2/sec; one conversion is 4.57
   # the other is, only 45% of solar radiation is PAR
+  
+  # KB, KD, Rso all match website; Ra is very very close
+  # converted value is ~1.5% different, probably due to rounding along the way?
   
   Rso * 4.57 * 0.45
 }
